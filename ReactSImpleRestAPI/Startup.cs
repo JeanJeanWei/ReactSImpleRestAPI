bf8450b77768f7dc3c44d1651791941c80c36ba9
Jean-Jean Wei <jeanjean.wei@gmail.com>
2021-06-10 12:42:08 +0800

    swagger screenshot  + document config

--- ReactSImpleRestAPI/Startup.cs
+++ ReactSImpleRestAPI/Startup.cs
@@ -30,7 +30,27 @@ namespace ReactSImpleRestAPI
             });
 
             // Register the Swagger services
-            services.AddSwaggerDocument();
+            services.AddSwaggerDocument(config =>
+            {
+                config.PostProcess = document =>
+                {
+                    document.Info.Version = "v1";
+                    document.Info.Title = "Color API (Backend for React Client)";
+                    document.Info.Description = "A simple ASP.NET Core web API";
+                    document.Info.TermsOfService = "None";
+                    document.Info.Contact = new NSwag.OpenApiContact
+                    {
+                        Name = "Jean-Jean Wei",
+                        Email = string.Empty,
+                        Url = "https://www.linkedin.com/in/jean-jean-wei-113a7244/"
+                    };
+                    //document.Info.License = new NSwag.OpenApiLicense
+                    //{
+                    //    Name = "Use under LICX",
+                    //    Url = "https://example.com/license"
+                    //};
+                };
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
